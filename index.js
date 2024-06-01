5a4b64df0f2d27e092e1e2a6c0cd14850f4b0de8
K.sridhar <142417968+Sridhar-K2@users.noreply.github.com>
2024-06-01 19:05:51 +0530

    first commit

--- index.js
+++ index.js
@@ -9,9 +9,8 @@ const cookieParser = require('cookie-parser');
 const router = require('./routes'); // Import router from separate file
 
 // Create an Express application instance
-const PORT = process.env.PORT || 5001;
 const app = express();
-app.use (cors())
+
 // Connect to MongoDB using Mongoose
 mongoose.connect(process.env.MONGO)
     .then(() => {
@@ -24,7 +23,6 @@ mongoose.connect(process.env.MONGO)
 
         // Set up middleware
         app.use(express.json()); // Parse JSON bodies
-        
        /* app.use(cors({
             //origin: 'http://localhost:3000',
             origin: 'https://comfy-mousse-86384e.netlify.app/', // Allow requests from this origin
@@ -32,28 +30,25 @@ mongoose.connect(process.env.MONGO)
             credentials: true // Allow cookies to be sent cross-origin
         }));
         app.use(cookieParser()); // Parse cookies */
-        // const allowedOrigins = ['http://localhost:3000', 'https://superb-mousse-933e54.netlify.app/'];
-       
-        // app.use(cors({
-            //origin: 'http://localhost:3000',
-            // origin: 'https://comfy-mousse-86384e.netlify.app/'
-    // app.use(cors({
-    //     origin: function (origin, callback) {
-    //         if (!origin || allowedOrigins.includes(origin)) {
-    //             callback(null, true); // Allow the request
-    //         } else {
-    //             callback(new Error('Not allowed by CORS')); // Block the request
-    //         }
-    //     },
-    //     methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
-    //     credentials: true // Allow cookies to be sent cross-origin
-    // }));
+        const allowedOrigins = ['http://localhost:3000', 'https://stately-marzipan-2671de.netlify.app/'];
+
+    app.use(cors({
+        origin: function (origin, callback) {
+            if (!origin || allowedOrigins.includes(origin)) {
+                callback(null, true); // Allow the request
+            } else {
+                callback(new Error('Not allowed by CORS')); // Block the request
+            }
+        },
+        methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
+        credentials: true // Allow cookies to be sent cross-origin
+    }));
 
         // Mount the router for API endpoints under "/api"
         app.use('/api', router);
 
         // Start the Express server
-         // Use specified port or default to 5000
+        const PORT = process.env.PORT || 5000; // Use specified port or default to 5000
         app.listen(PORT, () => {
             console.log(`Server is running on port ${PORT}`);
         });
@@ -62,4 +57,3 @@ mongoose.connect(process.env.MONGO)
     .catch((err) => {
         console.error('Error connecting to MongoDB:', err);
     });
-
