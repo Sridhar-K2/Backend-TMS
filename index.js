da04520b39d628ce2b570e679a87a19047fc1225
K.sridhar <142417968+Sridhar-K2@users.noreply.github.com>
2024-06-01 19:09:15 +0530

    first commit

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ mongoose.connect(process.env.MONGO)
             credentials: true // Allow cookies to be sent cross-origin
         }));
         app.use(cookieParser()); // Parse cookies */
-        const allowedOrigins = ['http://localhost:3000', 'https://stately-marzipan-2671de.netlify.app/'];
+        const allowedOrigins = ['https://stately-marzipan-2671de.netlify.app/'];
 
     app.use(cors({
         origin: function (origin, callback) {
